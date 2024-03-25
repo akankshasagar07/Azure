a6c6deeeefab242223fe9833683b48cafac25e48
akanksha.sagar <akanksha.sagar@denvergov.org>
2024-03-25 15:46:20 -0600

    corrected path

--- AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
+++ AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
@@ -7,8 +7,8 @@ param (
 
 $deploymentName = "Multi-sub-deployment"
 $deploymentLocation = "eastus2"
-$templateFile = ".\ARM-Templates\storage-Account\azuredeploy.json"
-$templateParameterFile = ".\ARM-Templates\storage-Account\azuredeploy.parameters.json"
+$templateFile = ".\ARM-Templates\storage-account\azuredeploy.json"
+$templateParameterFile = ".\ARM-Templates\storage-account\azuredeploy.parameters.json"
 #$templateFile = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY/_EpturaAZLHtest-CI/drop/s/multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.json"
 #$templateParameterFile = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY/_EpturaAZLHtest-CI/drop/s/multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.parameters.json"
 # If you have a set of subs that never should have deployments
