290411296f307f9a6ba0b08edab86f5e26fe2dc1
akanksha.sagar <akanksha.sagar@denvergov.org>
2024-03-25 21:20:23 -0600

    no pipeline yml

--- AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
+++ AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
@@ -5,12 +5,36 @@ param (
     $subscriptionId
 )
 
+Write-Host "Debug Information:"
+
 $deploymentName = "Multi-sub-deployment"
 $deploymentLocation = "eastus2"
 $templateFile = ".\ARM-Templates\storage-account\azuredeploy.json"
 $templateParameterFile = ".\ARM-Templates\storage-account\azuredeploy.parameters.json"
 #$templateFile = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY/_EpturaAZLHtest-CI/drop/s/multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.json"
 #$templateParameterFile = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY/_EpturaAZLHtest-CI/drop/s/multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.parameters.json"
+# Output the current working directory
+Write-Host "Current working directory: $(Get-Location)"
+
+# Construct the full paths to the template and parameters files
+$fullTemplateFilePath = Join-Path -Path $(Get-Location) -ChildPath $templateFile
+$fullTemplateParametersFilePath = Join-Path -Path $(Get-Location) -ChildPath $templateParameterFile
+
+# Output the full paths
+Write-Host "Template file path: $fullTemplateFilePath"
+Write-Host "Parameters file path: $fullTemplateParametersFilePath"
+
+# Check if the files exist at the constructed paths
+if (-Not (Test-Path -Path $fullTemplateFilePath)) {
+    Write-Host "Template file not found at path: $fullTemplateFilePath"
+    exit 1
+}
+if (-Not (Test-Path -Path $fullTemplateParametersFilePath)) {
+    Write-Host "Parameters file not found at path: $fullTemplateParametersFilePath"
+    exit 1
+}
+
+
 # If you have a set of subs that never should have deployments
 # But is available to the service principal
 $excludedSubs = (
