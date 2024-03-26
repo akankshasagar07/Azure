26cc4219f8adad69e1161454371a333d97541380
akanksha.sagar <akanksha.sagar@denvergov.org>
2024-03-26 12:53:24 -0600

    reverted changes in deploy.ps1

--- AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
+++ AzureLighthouse/multi-subscription-deployment/ARM-Templates/storage-account/deploy.ps1
@@ -1,22 +1,20 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)]
-    [string]$subscriptionId,
-
-    [Parameter(Mandatory = $true)]
-    [string]$workingDirectory
+    [Parameter()]
+    [string]
+    $subscriptionId
 )
 
 Write-Host "Debug Information:"
 Write-Host "Subscription ID: $subscriptionId"
-Write-Host "Working Directory: $workingDirectory"
+#Write-Host "Working Directory: $workingDirectory"
 
 $deploymentName = "Multi-sub-deployment"
 $deploymentLocation = "eastus2"
-# $templateFile = ".\ARM-Templates\storage-Account\azuredeploy.json"
-# $templateParameterFile = ".\ARM-Templates\storage-Account\azuredeploy.parameters.json"
-$templateFile = Join-Path -Path $workingDirectory -ChildPath "multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.json"
-$templateParameterFile = Join-Path -Path $workingDirectory -ChildPath "multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.parameters.json"
+$templateFile = ".\multi-subscription-deployment\ARM-Templates\storage-Account\azuredeploy.json"
+$templateParameterFile = ".\multi-subscription-deployment\ARM-Templates\storage-Account\azuredeploy.parameters.json"
+#$templateFile = Join-Path -Path $workingDirectory -ChildPath "multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.json"
+#$templateParameterFile = Join-Path -Path $workingDirectory -ChildPath "multi-subscription-deployment/ARM-Templates/storage-account/azuredeploy.parameters.json"
 
 Write-Host "Current working directory: $(Get-Location)"
 
